512aa7078871867b9530415a818bbc5bf4745937
Lathifa_Itqon <55240758+1234la@users.noreply.github.com>
2021-04-20 17:53:22 +0700

    Update soal2.c

--- soal2/soal2.c
+++ soal2/soal2.c
@@ -1,142 +1,206 @@
-#include <stdlib.h>
 #include <sys/types.h>
-#include <unistd.h>
+#include <sys/stat.h>
 #include <stdio.h>
-#include <wait.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
 #include <string.h>
+#include <wait.h>
 #include <dirent.h>
 
-void make_folder(char arr[]);
+void delete_file(char name[], char path[]);
 
-// void make_caption(char name[], char age[], char path[]){
-
-// }
-
-// void rename_file(char name[], char path[],char name_file[]){
-//     pid_t child_id;
-//     child_id = fork();
-//     int status;
-
-//     strcat(name,".jpg");
-
-//     if (child_id < 0) {
-//       exit(EXIT_FAILURE); 
-//     }
-
-//     if (child_id == 0) {
-//       //rename namefile
-//       char *argv[] = {"mv",name_file,name,NULL};
-//       execv("/bin/mv", argv);
-//     } else {
-//       while ((wait(&status)) > 0);
-//       return;
-//     }
-// }
-
-// void split_name_file(char name_file[],char path[]){
-//   char type[30];
-//   char name[30];
-//   char age[30];
-//   char *split = strtok(name_file, "_;"); 
-//   int word;
-//   while (split != NULL )
-//   {
-//     if(word == 1 | word == 4){ strcat(type,split);}
-//     if(word == 2 | word == 5){ strcat(name,split); rename_file(name,path,name_file);}
-//     if(word == 3 | word == 6){ strcat(age,split);} // make_caption(name,age);
-//   }
-//   return;
-// }
-
-// char *dir_content(char path){
-//   DIR *d = opendir(path); //open the path
-//   struct dirent * dir; //for the directory enteries
-//   char name_file[1000];
-//   char direction[] = "/home/lathifa/modul2/petshop/";
-  
-  
-//   while ((dir = readdir(d)) != NULL){
-//     //condition to check regular file
-//     name_file[0]='\0';
-//     strcat(name_file,path);
-//     strcat(name_file, dir->d_name);
-//     split_name_file(name_file,direction);
-//   }
-//   return name_file;
-//   closedir(d);
-// }
-
-void move_file(char arr[], char path[]){
+void make_folder(char pet_type[], char path[]) {
     pid_t child_id;
+    int status;
+    char path_temp[100]="";
+
+    strcpy(path_temp, path);
+    strcat(path_temp, pet_type);
+
+    //strcat(path,pet_type);
+
     child_id = fork();
+
+    if (child_id < 0) {
+      exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+     }
+
+     if (child_id == 0) {
+       // this is child
+
+       char *argv[] = {"mkdir", "-p", path_temp, NULL};
+       execv("/bin/mkdir", argv);
+      } else {
+        while((wait(&status)) > 0);
+        return;
+      }
+}
+
+void move_file(char name[], char pet_type[], char pet_name[], char path[]) {
+    pid_t child_id;
     int status;
 
-    char name_file[100]= "";
-    //memcpy(&name_file,&arr,sizeof name_file);
-    strcat(name_file,arr);
-    char simbol[] = "";
+    child_id = fork();
+
+    //path file sebelum
+    char path_file[100] = "";
+    strcpy(path_file, path);
+    strcat(path_file, name);
+
+    
+    //buat path  file pada folder type
+    char path_folder[100] = "";
+    strcpy(path_folder, path);
+    strcat(path_folder, pet_type);
+
+    //ganti nama file
+    strcat(path_folder,"/");
+    strcat(path_folder, pet_name);
+    strcat(path_folder,".jpg");
  
-    strcat(name_file,"*");
 
     if (child_id < 0) {
-    exit(EXIT_FAILURE); 
-    }
+      exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
+     }
 
-    if (child_id == 0) {
-      //memindahkan file
-      char *argv[] = {"find","/home/lathifa/modul2/petshop/", "-type","f","-name",name_file,"-exec","mv","-t",path,"{}","+",NULL};//-t <targetdir>
-      execv("/usr/bin/find", argv);
-    } else {
-      while ((wait(&status)) > 0);
-          // pid_t child2_id;
-          // child2_id = fork();
+     if (child_id == 0) {
+       // this is child
 
-          // if(child2_id <0){
-          //   exit(EXIT_FAILURE); 
-          // }
-          // if (child_id == 0) {
-          //   dir_content(path);
-          // }else{
-          //   while((wait(&status)) > 0);
-          //     return;
-          // }
-          return;
-    }
+       char *argv[] = {"cp", path_file, path_folder, NULL};
+       execv("/bin/cp", argv);
+      } else {
+        while((wait(&status)) > 0);
+        return;       
+      }
 }
 
-    
+void make_caption(char pet_type[], char pet_name[], char pet_age[], char path[]) {
+   
+    char path_file[100] = "";
 
-void make_folder(char arr[]){
+    strcat(path_file, path);
+    strcat(path_file, pet_type);
+    strcat(path_file, "/keterangan.txt");
+
+    FILE *fp;
+    fp = fopen (path_file, "a");
+    fprintf(fp, "nama: %s\numur: %s tahun\n\n", pet_name, pet_age);
+
+    fclose(fp);
+    return;
+}
+
+void delete_file(char name[], char path[]){
+    
+    char new_path[100] = "";
+    strcat(new_path, path);
+    strcat(new_path, name);
+//     printf("fungsi delete_file\n");
+//     printf("%s\n\n",new_path);
+
     pid_t child_id;
     child_id = fork();
+
     int status;
 
-    char path[] = "/home/lathifa/modul2/petshop/";
-    char directory[100] = "";
-    strcat(directory,path);
-    strcat(directory,arr);
-    
     if (child_id < 0) {
-    exit(EXIT_FAILURE); 
+        exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
     }
 
     if (child_id == 0) {
-        //membuat folder 
-        char *argv[] = {"mkdir","-p", directory, NULL};
-        execv("/bin/mkdir", argv);
-
-    } else {
-        while ((wait(&status)) > 0);
-        move_file(arr,directory);
+        char *argv[] = {"rm", "-r", new_path, NULL};
+        execv("/bin/rm", argv);
+    }
+    else{
+        while ((wait(&status)) > 0)
+        {
+            return;
+        }
+        
     }
 }
+
+void delete_jpg(char *str, char *substr){
+    char *dot_jpg;
+    dot_jpg = strstr(str,substr); // strstr untuk menemukan sebuah teks pada string //menemukan substr dalam str
+    if (dot_jpg != NULL) {
+        int pos = dot_jpg - str;
+
+//         printf("fungsi delete_jpg\n");
+//         printf("str     : %s\n", str);
+//         printf("dot_jpg : %s\n", dot_jpg);
+//         printf("pos     : %d\n\n", pos);
+//         sprintf(str, "%.*s", pos, str);
+        // return pos;
+    }
+}
+
+void split_name(char name[]){
+        char path[] = "/home/lathifa/modul2/petshop/";
+        char file[100];
+        
+        strcpy(file,name);
+        //delete_jpg(file, ".jpg");
+        
+        char pet_type[30];
+        char pet_name[30];
+        char pet_age[30];
+        char *split ;
+        split = strtok(file, "_;");
+    
+        int word = 0;
+        while(split != NULL) {
+
+        if(word == 0 | word == 3) { strcpy(pet_type, split); make_folder(pet_type, path); }
+
+        if(word == 1 | word == 4) { strcpy(pet_name, split); move_file(name, pet_type,pet_name, path);}
+
+        if(word == 2 | word == 5) { strcpy(pet_age, split); delete_jpg(pet_age,".jpg"); make_caption(pet_type, pet_name, pet_age, path);}
+
+        split = strtok(NULL, "_;");
+        word++;
+    }
+//     printf("fungsi split_name\n");
+//     printf("type: %s\n",pet_type);
+//     printf("name: %s\n",pet_name);
+//     printf("age : %s\n",pet_age);
+//     printf("path: %s\n\n",path);
+//     delete_file(name,path);
+}
+
+
+void dir(){
+    char path[] = "/home/lathifa/modul2/petshop/";
+
+    DIR *dir= opendir(path);
+    struct dirent *dp;
+
+    char name[100];
+
+    if (dir != NULL)
+    {
+      while ((dp = readdir (dir))) {
+          
+        if(strcmp(dp->d_name, ".") !=0 && strcmp(dp->d_name, "..") !=0) {
+           
+            strcpy(name,dp->d_name);
+            split_name(name);     
+        }
+      }
+     closedir(dir);
+    }
+  return ;
+}
+
 
 int main() {
   pid_t child_id;
   child_id = fork();
-  
+
   int status;
-  char *arr[] = {"cat","dog","parrot","rabbit","guinea","betta","chicken","frog","hamster","iguana","ilama","otter","racoon","sheep","tiger"};
   
   if (child_id < 0) {
     exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
@@ -192,14 +256,9 @@ int main() {
 
 			  } else {
 			    while ((wait(&status)) > 0);
-                    for(int i=0; i<15;i++)
-                        make_folder(arr[i]);
+                    dir();
                 }
             }
         }
     }
 }
-
-
-
-
